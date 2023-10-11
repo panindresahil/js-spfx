5919a67e0b57443d74925d8cf44532235958d72b
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-11 08:15:56 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert("Hello") // We are using NodeJs, not browser.
+
+let name = 'Sahil'
+let age = 24
+let isLoggedIn = false
+
+/* 
+number => 2^53
+bigint
+strint => "", ''
+boolean => true/false
+null => standalone value
+undefined => not defined
+symbol => unique
+*/
+
+/* 
+Object
+*/
+
+console.log(typeof "Sahil");
+console.log(typeof true);
+console.log(typeof 43);
+console.log(typeof undefined);
+console.log(typeof null);
