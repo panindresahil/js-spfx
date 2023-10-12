952af3b021ea7e4d0f5cc13700222f4177160eec
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-12 06:05:40 +0000

    Comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+
+console.log(null == 0);
+
+// Comparison operators convert null to zero. 
+
+// === -> Strict Check
+
+console.log("2" === 2);
