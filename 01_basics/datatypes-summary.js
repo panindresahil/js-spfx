ca68367d9b8c015c53974e48b81a47c2d05edbc9
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-12 06:22:25 +0000

    Data Type Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+// Primitive - Call By Value
+
+/*
+7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt
+*/ 
+
+
+// Non Primitive - Call By Reference
+/* 
+Array, Objects, Functions
+*/
