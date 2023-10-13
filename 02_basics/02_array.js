34a8e9066537e5526f840f112cf021822b93f208
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-13 15:29:58 +0000

    Arrays-2

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -18,4 +18,20 @@ const anotherArray = [1, 2, 3, [4, 5], 6, [7, [8, 9], 10]]
 
 const realAnotherArray = anotherArray.flat(Infinity)
 
-console.log(realAnotherArray);
+// console.log(realAnotherArray);
+
+console.log(Array.isArray('Sahil'));
+
+console.log(Array.from('Sahil'));
+
+console.log(Array.from({name: 'Sahil'}));
+
+let score = 100
+
+let score2 = 200
+
+let score3 = 300
+
+console.log(Array.of(score, score2, score3))
+
+
