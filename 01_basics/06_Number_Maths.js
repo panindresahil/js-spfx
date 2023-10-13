2e953b8dc9b48cf87569177190daad471f8a2f8e
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-13 06:31:19 +0000

    Number & Math Module

---
+++ 01_basics/06_Number_Maths.js
@@ -0,0 +1,55 @@
+const score = 400
+
+// console.log(score);
+
+const balance = new Number(8500)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+const amount = 75.6879
+
+// console.log(amount.toFixed(2));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+
+// console.log(Number.MIN_VALUE);
+
+
+// ------------------------- Maths --------------------------------
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.4));
+
+// console.log(Math.ceil(4.3));
+
+// console.log(Math.floor(4.6));
+
+// console.log(Math.sqrt(9));
+
+// console.log(Math.pow(2, 2));
+
+// console.log(Math.min(23,56,78,345));
+
+// console.log(Math.max(23,56,78,345));
+
+let randNum = Math.random()
+
+console.log(Math.ceil(randNum * 10));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+
+
+
+
+
