6bb4d2a9f7430164f222250f1a80296a8735bf59
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-18 01:46:24 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,9 +8,47 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log('inner', a);
+    // console.log('inner', a);
 }
 
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+
+function one() {
+    const userName = 'Sahil'
+
+    function two() {
+        const website = 'youtube'
+        // console.log(userName);
+    }
+    // console.log(website);
+
+    two()
+}
+
+one()
+
+if (true) {
+    const userName = 'Sahil'
+    if (true) {
+        const website = 'youtube'
+        // console.log(userName + ' ' + website)
+    }
+    // console.log(website);
+}
+// console.log(userName);
+
+// ******************************* Interesting ************************************
+// console.log(addone(3)); // In this case funtion can be accessed any where.
+
+function addone(num) {
+    return num + 1
+}
+
+// console.log(addTwo(3)); // In this case function cannot be accessed.
+
+const addTwo = function(num) {
+    return num + 2
+}
