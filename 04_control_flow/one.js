9206e3dca6304f3715c50355f45de3677cf8bb70
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-18 16:34:06 +0000

    Conditions

---
+++ 04_control_flow/one.js
@@ -0,0 +1,11 @@
+// If Statement
+const num = 6
+const temperature = 30
+if (temperature < 35) {
+    console.log(`Temperature is less than 35`);
+} else {
+    console.log(`Temperature is greater than 35`);
+}
+// Operators
+
+// <, >, <=, >=, ==, !=, ===, !==
