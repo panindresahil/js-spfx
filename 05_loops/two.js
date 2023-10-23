c576f9c814874a1ed782801031365e7d8ab29aed
Sahil Panindre <46246306+panindresahil@users.noreply.github.com>
2023-10-23 05:22:50 +0000

    while, do-while

---
+++ 05_loops/two.js
@@ -0,0 +1,13 @@
+let num = 0
+
+let heroes = ['Iron Man', 'Thor', 'Hulk', 'Bat Man', 'Super Man']
+
+// while (num < heroes.length) {
+//    console.log(`Heroes #${num + 1}:- ${heroes[num]}`); 
+//    num++
+// }
+
+do {
+    console.log(`Heroes #${num + 1}:- ${heroes[num]}`); 
+    num++
+} while (num < heroes.length);
